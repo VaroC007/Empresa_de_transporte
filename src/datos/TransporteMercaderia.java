3f30bea008df6bcad52a585ec4f9bf6990287691
Julian Novoa <Julian Novoa@LAPTOP-C5CR0PGL>
2025-09-11 17:13:43 -0300

    Creacion de clases e interface en paquete datos

--- src/datos/TransporteMercaderia.java
+++ src/datos/TransporteMercaderia.java
@@ -4,10 +4,73 @@
  */
 package datos;
 
+import tpg2_prog_2025.Consola;
+
 /**
  *
  * @author Julian Novoa
  */
-public class TransporteMercaderia {
-    
+public class TransporteMercaderia extends Transporte implements ICalculable {
+
+    private double toneladas;
+    private boolean esPeligroso;
+
+    public TransporteMercaderia() {
+        super();
+        this.toneladas = 0.0;
+        this.esPeligroso = false;
+    }
+
+    public double getToneladas() {
+        return toneladas;
+    }
+
+    public boolean isEsPeligroso() {
+        return esPeligroso;
+    }
+
+    public void setToneladas(double toneladas) {
+        if (toneladas < 0) {
+            throw new IllegalArgumentException("Las toneladas no pueden ser negativas.");
+        }
+        this.toneladas = toneladas;
+    }
+
+    public void setEsPeligroso(boolean esPeligroso) {
+        this.esPeligroso = esPeligroso;
+    }
+
+    private void cargarToneladas() {
+        double t;
+        do {
+            Consola.emitirMensaje("Toneladas transportadas: ");
+            t = Consola.leerDouble();
+        } while (t <= 0);
+        this.toneladas = t;
+    }
+
+    private void cargarEsPeligroso() {
+        char resp;
+        do {
+            Consola.emitirMensaje("¿Es mercadería peligrosa? (S/N): ");
+            resp = Consola.leerChar();
+        } while (!(resp == 'S' || resp == 's' || resp == 'N' || resp == 'n'));
+
+        this.esPeligroso = (resp == 'S' || resp == 's');
+    }
+
+    @Override
+    public double calcularExtra() {
+        double base = 7000 * toneladas;
+        if (esPeligroso) {
+            base += 20000; // adicional fijo
+        }
+        return base;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Toneladas=" + toneladas + ", Peligroso=" + esPeligroso
+                + ", Extra calculado=$" + calcularExtra();
+    }
 }
--- src/datos/TransportePersonas.java
+++ src/datos/TransportePersonas.java
@@ -4,10 +4,61 @@
  */
 package datos;
 
+import tpg2_prog_2025.Consola;
+
 /**
  *
  * @author Julian Novoa
  */
-public class TransportePersonas {
+public class TransportePersonas extends Transporte implements ICalculable{
+
+    private int personas;
+
+    public TransportePersonas() {
+        super();
+        this.personas = 0;
+    }
+
+    public int getPersonas() {
+        return personas;
+    }
+
+    private void setPersonas(int personas) {
+        if (personas < 0) {
+            throw new IllegalArgumentException("La cantidad de personas no puede ser negativa.");
+        }
+        this.personas = personas;
+    }
+
+    private void cargarPersonas() {
+        int p;
+        do {
+            Consola.emitirMensaje("Cantidad de personas: ");
+            p = Consola.leerInt();
+        } while (p <= 0);
+        this.personas = p;
+    }
     
+    @Override
+    public void cargarDatos(){
+        super.cargarDatos();
+        cargarPersonas();
+        this.extra=calcularExtra();
+    }
+    
+    @Override
+    public double calcularExtra(){
+        if(personas>9){
+            return 5500*this.horas;
+        }else{
+            return 3000*this.horas;
+        }
+    }
+    
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", Personas=" + personas + ", Extra calculado=$" + calcularExtra();
+    }
+
 }
